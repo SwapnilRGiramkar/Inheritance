0b93f9937b4ea248b354b9ab54d6a2c55dbbe52f
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-06-25 23:07:56 +0530

    University,College and Department example of Multi-level-Inheritance

---
+++ Multi-Level-Inheritance.java
@@ -0,0 +1,140 @@
+//Multi Level Inheritance using example University College and Department
+package inheritance;
+//Multi level Inheritance
+public class University 
+{
+	String uName;
+	String uType;
+	String vc;
+	String uLoc;
+	int code;
+	
+	public University() {
+		// TODO Auto-generated constructor stub
+	}
+	University(String uName,String uType,String vc,String uLoc,int code)
+	{
+		this.uName=uName;
+		this.uType=uType;
+		this.vc=vc;
+		this.uLoc=uLoc;
+		this.code=code;
+	}
+	public void displayUniversity()
+	{
+		System.out.println("University Name : "+uName);
+		System.out.println("University Type : "+uType);
+		System.out.println("Voice chancellor : "+vc);
+		System.out.println("University Location : "+uLoc);
+		System.out.println("University Code : "+code);
+		System.out.println("-----------------------------------");
+	}
+
+}
+//--------------------------------------------------------------------------------------
+package inheritance;
+//Multi level Inheritance
+public class College extends University
+{
+	String collName;
+	String principal;
+	String address;
+	int collCode;
+	int grade;
+	int noOfDepts;
+	
+	public College() {
+		// TODO Auto-generated constructor stub
+	}
+	College(String uName,String uType,String vc,String uLoc,int code,String collName,String principal,String address,
+			int collCode,int grade,int noOfDepts)
+	{
+		this.uName=uName;
+		this.uType=uType;
+		this.vc=vc;
+		this.uLoc=uLoc;
+		this.code=code;
+		
+		this.collName=collName;
+		this.principal=principal;
+		this.address=address;
+		this.collCode=collCode;
+		this.grade=grade;
+		this.noOfDepts=noOfDepts;
+		
+	}
+	public void displayCollege()
+	{
+		displayUniversity();
+		System.out.println("College Name : "+collName);
+		System.out.println("Principal : "+principal);
+		System.out.println("College Address : "+address);
+		System.out.println("College Code : "+collCode);
+		System.out.println("Grade : "+grade);
+		System.out.println("No of Departnents : "+noOfDepts);
+		System.out.println("---------------------------------");
+	}
+
+}
+//------------------------------------------------------------------------------
+package inheritance;
+//Multi level Inheritance
+public class Department extends College
+{
+	String type;
+	String hod;
+	int lec;
+	int noOfStudents;
+	int seats;
+	
+	public Department() {
+		// TODO Auto-generated constructor stub
+	}
+	Department(String uName,String uType,String vc,String uLoc,int code,String collName,String principal,String address,
+			int collCode,int grade,int noOfDepts,String type,String hod,int lec,int noOfStudents,int seats)
+	{
+		this.uName=uName;
+		this.uType=uType;
+		this.vc=vc;
+		this.uLoc=uLoc;
+		this.code=code;
+		
+		this.collName=collName;
+		this.principal=principal;
+		this.address=address;
+		this.collCode=collCode;
+		this.grade=grade;
+		this.noOfDepts=noOfDepts;
+		
+		this.type=type;
+		this.hod=hod;
+		this.lec=lec;
+		this.noOfStudents=noOfStudents;
+		this.seats=seats;
+	}
+	public void displayDepartment()
+	{
+		displayCollege();
+		System.out.println("Type : "+type);
+		System.out.println("HOD : "+hod);
+		System.out.println("Lecturer : "+lec);
+		System.out.println("Students Count : "+noOfStudents);
+		System.out.println("Seats : "+seats);
+	}
+
+}
+//---------------------------------------------------------------------------------
+package inheritance;
+//Multi level Inheritance
+public class UniversityDriver 
+{
+	public static void main(String[] args) 
+	{
+		Department d1=new Department("SPPU","Technological","Suresh Gosavi","Pune",4093,"APCOER",
+				"Sunil Thakare","Pune",4568,2,7,"CS","Kalal",20,300,300);
+		
+		d1.displayDepartment();
+	}
+
+}
+
